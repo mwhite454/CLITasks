f41f5d63a31f12a8ffa51add8739942d3e5f4b6e
mwhite454 <mwhite454@gmail.com>
2016-11-21 15:46:33 -0600

    REFACTOR
    Major changes to make tasks.js persist at the prompt and request further
    actions after each add/update/archive

--- ReadMe.md
+++ ReadMe.md
@@ -6,11 +6,6 @@ Don't want to take your hands off the keyboard to track tasks? I admire your com
 Requires a running instance of [Node.js](https://nodejs.org/en/)
 Download and NPM Install
 
-Options:
-* node tasks.js - this will show the current tasks stored in file
-* node tasks.js add - this will prompt you to add a new task and priority
-* node tasks.js done - show you current active tasks and prompt you for an ID to mark as completed
-
 All tasks are stored in JSON format for ease of access and legibility.
 Stores and tracks the start/complete date of each task.
 Status=0 is incomplete, while status=1 is complete.
--- tasks.js
+++ tasks.js
@@ -1,16 +1,35 @@
-  var prompt = require('prompt');
-  var fs = require('fs');
-  var tasks = [];
-  var nowish = new Date();
-  var logFile = './tasks.json';
-  var logJSON = {id: 0, title: "", priority: "", status: "", created:"", completed: ""};
+var prompt = require('prompt')
+var fs = require('fs');
+var colors = require("colors");
+var tasks = [];
+var nowish = new Date();
+var logFile = './tasks.json';
+var archiveFile = './archive.json';
+var logJSON = {id: 0, title: "", priority: "", status: "", created:"", completed: ""};
+var maxID = 0;
+
+
+prompt.delimiter = colors.cyan(">>");
+prompt.start();
 
 String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    };
+}
+
 function logTask(task, priority){
-  logJSON.id = tasks.length + 1;
+  logJSON.id = maxID + 1;
   logJSON.title = task;
   logJSON.priority = priority;
   logJSON.status = 0;
@@ -23,45 +42,145 @@ function logTask(task, priority){
 function updateLog(){
     fs.writeFile(logFile, JSON.stringify(tasks), function(err) {
     if (err) throw err;
-    console.log('It\'s saved!');
+    console.log(colors.red('It\'s saved!'));
+    readTasks(logFile);
   });
 }
 
-function readTasks(file){
+function getDays(date){
+  var age = Math.floor((nowish - date) / (1000*60*60*24));
+  return age;
+}
+
+function getDaysMsg(date){
+  var age = getDays(date);
+  var ageMsg = "";
+  if (age < 1) {
+    ageMsg = "Less than a day old.";
+  } else if (age < 2 ) {
+    ageMsg = "About a day old.";
+  } else {
+    ageMsg = age + " days old.";
+    if(age>3 && age<10){
+      ageMsg = colors.yellow(ageMsg);
+    } else if (age>=10){
+      ageMsg = colors.red(ageMsg);
+    }
+  }
+  return ageMsg;
+}
+
+function fillArchive(){
+   tasks = JSON.parse(fs.readFileSync(logFile, 'utf8'));
+   var toArchive = [];
+   var toKeep = [];
+   for(var i=0; i<tasks.length; i++){
+     var task = JSON.parse(JSON.stringify(tasks[i]));
+     if(task.status === 0){
+       toKeep.push(task);
+     } else {
+       toArchive.push(task);
+     }
+   }
+  tasks = toKeep;
+  fs.writeFile(archiveFile, JSON.stringify(toArchive), function(err) {
+    if (err) throw err;
+    console.log(toArchive.length + " items Archived. " + toKeep.length + " tasks remain.");
+  });
+  updateLog();
+}
+
+function readTasks(file, statusType){
+ var counter = 0;
+ var oldMax = 0;
+ if(statusType === undefined){
+   statusType = 0;
+ } else {
+   statusType = 1;
+ }
+
  tasks = JSON.parse(fs.readFileSync(file, 'utf8'));
+ if (tasks.length > 1){
+ tasks = tasks.sort(dynamicSort("ID"));
     for(var i=0; i<tasks.length; i++){
       var task = JSON.parse(JSON.stringify(tasks[i]));
-      if(task.status === 0){
+      var response = "";
+      oldMax = Math.max(oldMax, task.id);
+      if (maxID < oldMax){
+        maxID = oldMax;
+      }
+      if(task.status === statusType){
+        counter +=1;
         var currStatus = "Incomplete";
         var displayDate = new Date(task.created);
-        console.log("Task ID: " + task.id + " | " + task.title.toProperCase() + " | Status: " + currStatus + " | Started on: " + displayDate.toLocaleString());
+        response = "ID: " + task.id + " | " + task.title.toProperCase() + " | " + getDaysMsg(displayDate);
+        if (task.priority === 1){
+          console.log(colors.bgRed.black(response));
+        } else if(task.priority === 2){
+          console.log(colors.bgWhite.black(response));
+        } else {
+          console.log(colors.white(response));
+        }
       }
-
     }
-    //callPrompt();
+    //console.log(counter + " of " + tasks.length + " are incomplete.");
+  }
+    callPromptQuery();
+}
+
+function callPromptQuery(){
+  prompt.message = colors.cyan("What would you like to do? \n\r  (add) New Task \n\r  (done:id) Complete Specified Task ID \n\r  (see) See Current Tasks - see:1 to see completed tasks \n\r  (archive) To Move Completed Items to backup file \n\r  (stop) End Process \n\r");
+  var actionSchema = {
+    properties: {
+      action: {
+                description: colors.cyan("What's Next?"),
+                type: 'string',
+                message: colors.red("Type stop if you'd like to stop task.js \n\r Otherwise provide an action to perform..."),
+                required: true}
+    }
+  };
+  prompt.get(actionSchema, function (err, result) {
+    var req = result.action.split(":");
+    //route the action
+      if (req[0] == "add"){
+        callPromptAdd();
+      } else if (req[0] == "done"){
+        completeTask(req[1]);
+      }else if (req[0] == "see"){
+        readTasks(logFile, req[1]);
+      } else if (req[0] == "stop"){
+
+      } else if (req[0] == "archive"){
+        fillArchive();
+      } else {
+        console.log(colors.bgRed.white("I didn't get that..."));
+        callPromptQuery();
+      }
+  });
 }
 
 function callPromptAdd(){
-    prompt.start();
-    prompt.get(['task', 'priority'], function (err, result) {
-      //
-      // Log the results.
-      //
+    prompt.message = "<-Task.js->";
+    var schema = {
+      properties: {
+      task: {
+        description: colors.cyan("Let's add a task"),
+        type: 'string',
+        message: colors.red("Can't make an empty task. You can't do nothing!"),
+        required: true},
+      priority: {
+        description: colors.red(" 1 - High, 2 - Medium, 3 - Low"),
+        type: 'number',
+        required: true,
+        default: 3
+      }
+    }
+    };
+    prompt.get(schema, function (err, result) {
       logTask(result.task, result.priority);
-      console.log('Command-line input received:');
-      console.log('  task: ' + result.task);
-      console.log('  priority: ' + result.priority);
     });
 }
 
-function callPromptDone(){
-    prompt.start();
-    console.log('Which task ID is finished?');
-    prompt.get('id', function (err, result) {
-      completeTask(result.id);
-
-    });
-}
 
 function completeTask(sentID){
       for(var i=0; i<tasks.length; i++){
@@ -74,17 +193,4 @@ function completeTask(sentID){
       updateLog();
 }
 
-  if(process.argv.length < 3){
-    //console.log(process.argv.length);
-    readTasks(logFile);
-  } else if (process.argv[2] === 'add'){
-    readTasks(logFile);
-    callPromptAdd();
-  } else if (process.argv[2] === 'done'){
-    readTasks(logFile);
-    callPromptDone();
-  }
-/*process.argv.forEach((val, index) => {
-
-    console.log(`${index}: ${val}`);
-});*/
+readTasks(logFile);
